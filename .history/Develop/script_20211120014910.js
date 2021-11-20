60f6b215b612a0bc7bf4742d349fc3a73f028151
brenhamp <bpozzi@trinity.edu>
2021-11-20 02:26:03 -0600

    added variables for user prompts for password criteria

---
+++ .history/Develop/script_20211120014910.js
@@ -0,0 +1,21 @@
+// Assignment code here
+var passwordLength = prompt ("Enter desired password length between 8-128 (inclusive)");
+
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
---
+++ .history/Develop/script_20211120021206.js
@@ -0,0 +1,40 @@
+// Assignment code here
+
+
+
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+    
+//ask for a valid password length and console.log the user's input
+
+  function passwordSizeQuestion() {
+
+  var passwordSize = window.prompt ("Enter desired password length between 8-128 (inclusive)");
+
+  passwordSize;
+
+  if (passwordSize >=8 || passwordSize <=128 ) {
+    console.log(passwordSize);
+  }
+  else
+  {
+    window.alert ("Invalid input. Please enter a number between 8 and 128 (inclusive)");
+    passwordSizeQuestion();
+}
+
+  };  
+};
+
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
